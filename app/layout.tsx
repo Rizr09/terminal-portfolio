0cedaa9de849ca4c5befb61b761d13e7b3617685
Rizr09 <rizky2004cool@gmail.com>
2025-04-04 16:03:58 +0700

    feat: added favicon & desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,13 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Rizky\'s Portfolio',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
+  description: 'Created with 💖',
 }
 
 export default function RootLayout({
